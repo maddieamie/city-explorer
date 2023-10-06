a709cc2651506a9d38b2f271d23582ed8021cba4
Maddie Lewis <maddieamie@gmail.com>
2023-10-06 14:38:01 -0700

    update file path

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import '..css/index.css'
 // Import our custom CSS
 import '../scss/styles.scss'
 
