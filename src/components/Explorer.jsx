8f142f4857ae471c361d9cae5d9cba0c9e5a83fc
Maddie Lewis <maddieamie@gmail.com>
2023-10-14 14:25:53 -0700

    Updated Code for map icon.

--- src/components/Explorer.jsx
+++ src/components/Explorer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Container, Form } from "react-bootstrap";
+import { Button, Container, Form, ListGroup, Image } from "react-bootstrap";
 import axios from "axios";
 
 let API = import.meta.env.VITE_LOCATIONIQ_API_KEY;
@@ -10,18 +10,30 @@ class Explorer extends React.Component {
     this.state = {
       displayContent: false,
       searchQuery: '',
-      city: {}
+      city: {},
+      mapurl: ''
+      
     }
   }
 // yay 
   handleSearch = async (e) => {
     e.preventDefault();
     let url = `https://us1.locationiq.com/v1/search?key=${API}&q=${this.state.searchQuery}&format=json`;
-    // console.log(url)
-    const response = await axios.get(url);
-    console.log(response.data)
-    
-    this.setState({ city: response.data[0], displayContent: true })
+
+  axios.get(url)
+    .then(response => {
+      const cityData = response.data[0];
+
+      this.setState({ city: cityData, displayContent: true });
+
+      
+      const mapurl = `https://maps.locationiq.com/v3/staticmap?key=${API}&center=${cityData.lat},${cityData.lon}&zoom=11&size=450x450&format=json&maptype=png&markers=icon:small-purple-cutout|${cityData.lat},${cityData.lon}`;
+      this.setState({ mapurl }); 
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  
   }
 
 
@@ -30,14 +42,20 @@ class Explorer extends React.Component {
       <Container>
         <Form onSubmit={this.handleSearch}>
           <Form.Control onChange={(e) => this.setState({ searchQuery: e.target.value })} type="text" placeholder="Enter city...." />
-          <Button type='submit' >Explore!</Button>
+          <Button type='submit' variant="flat" size="xxl" variant="info" >Explore!</Button>
         </Form>
 
         {Object.keys(this.state.city).length > 0 &&
 
           <>
-            <p>{this.state.city.display_name}</p>
-            <p>Lat: {this.state.city.lat}, Lon: {this.state.city.lon}</p>
+          <Image src={this.state.mapurl} alt={this.state.city.display_name} roundedCircle />
+          <ListGroup>
+            <ListGroup.Item variant="success">{this.state.city.display_name}</ListGroup.Item>
+            <ListGroup.Item variant="warning">Lat: {this.state.city.lat}</ListGroup.Item>
+            <ListGroup.Item variant="info">Lon: {this.state.city.lon}</ListGroup.Item>
+              
+          </ListGroup>
+            
           </>
         }
 
